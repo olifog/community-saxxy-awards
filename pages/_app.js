ada52e9b9a68287647885e54c385edd4721c321a
Oli Fog <oliver@fogelin.com>
2021-03-08 18:01:40 +0000

    progress loader

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,14 @@ import Image from 'next/image'
 import Navbar from '../components/Navbar/Navbar'
 import PropTypes from 'prop-types'
 
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+NProgress.configure({ showBar: false })
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
 export default function App ({ Component, pageProps }) {
   return (
     <>
