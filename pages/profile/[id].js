4ab9dce6a195d2dcef3daad183b9a16be3a12cf7
Oli Fog <oliver@fogelin.com>
2021-03-09 20:47:25 +0000

    (slightly) more complete profile

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 import BounceLoader from 'react-spinners/BounceLoader'
+import Flag from 'react-flagkit'
 
 import dbConnect from '../../lib/dbConnect'
 import User from '../../models/User'
@@ -10,14 +12,40 @@ export default function Profile ({ user }) {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col justify-center h-screen">
+    <div className="flex flex-col items-center h-screen w-full max-w-screen-md">
       {router.isFallback
         ? (
             <BounceLoader color="#ffffff" size={150} />
           )
         : (
-            <div className="text-white text-lg z-10">
-              {user.name}
+            <div className="flex flex-col items-center transform translate-y-1/4 h-full w-full">
+              <div className="absolute transform skew-y-6 bg-gray-100 h-full w-full z-2"></div>
+              <div className="flex flex-col items-center transform -translate-y-24 space-y-1">
+                <div className="h-40 w-40 relative z-3">
+                  <a href={user.profileUrl}>
+                    <Image
+                      src={user.imageUrl}
+                      alt="User"
+                      layout="fill"
+                      objectFit="contain"
+                      className="rounded-full shadow-sm"
+                    />
+                  </a>
+                </div>
+                <div className="z-10 space-x-1">
+                  <a href={user.profileUrl}>
+                    <span className="inline-block text-gray-800 text-2xl font-semibold hover:text-gray-600">
+                      {user.name}
+                    </span>
+                  </a>
+                  <span className="inline-block">
+                    <Flag country={user.countryCode} size={26} />
+                  </span>
+                </div>
+              </div>
+              <div className="transform -translate-y-6 border-dotted border-4 border-gray-300 rounded-lg text-gray-400 text-sm z-10 w-5/6 p-3">
+                No Submission!
+              </div>
             </div>
           )
       }
