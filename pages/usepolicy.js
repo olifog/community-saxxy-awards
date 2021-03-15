471487b4fa4be529d9d6e2eda249366ecf1ea10e
Oli Fog <oliver@fogelin.com>
2021-03-15 19:07:20 +0000

    Acceptable use policy

---
+++ pages/usepolicy.js
@@ -0,0 +1,25 @@
+export default function UsePolicy () {
+  return (
+    <div className="flex flex-col max-w-screen-md px-8 pt-20 pb-8 items-start">
+      <div className="text-gray-200 z-10 space-y-4">
+        <h1 className="text-2xl font-bold">Acceptable Use Policy</h1>
+        <p>Please read this acceptable use policy (&quot;policy&quot;, &ldquo;AUP&rdquo;) carefully before using the saxxys.com website (&ldquo;website&rdquo;, &quot;service&quot;) operated by Community Saxxy Awards (&quot;us&quot;, &apos;we&quot;, &quot;our&quot;).</p>
+        <p>Services provided by us may only be used for lawful purposes. You agree to comply with all applicable laws, rules, and regulations in connection with your use of the services. Any material or conduct that in our judgment violates this policy in any manner may result in suspension or termination of the services or removal of the user&rsquo;s account with or without notice.</p>
+        <h2 className="text-lg font-semibold">Prohibited use</h2>
+        <p>You may not use the services to publish content or engage in activity that is illegal under applicable law, that is harmful to others, or that would subject us to liability, including, without limitation, in connection with any of the following, each of which is prohibited under this AUP:</p>
+        <ul>
+            <li>Phishing or engaging in identity theft</li>
+            <li>Distributing computer viruses, worms, trojan horses or other malicious code</li>
+            <li>Distributing pornography or adult related content or offering any escort services</li>
+            <li>Promoting or facilitating violence or terrorist activities</li>
+            <li>Infringing the intellectual property or other proprietary rights of others</li>
+        </ul>
+        <h2 className="text-lg font-semibold">Enforcement</h2>
+        <p>Your services may be suspended or terminated with or without notice upon any violation of this policy. Any violations may result in the immediate suspension or termination of your account.</p>
+        <h2 className="text-lg font-semibold">Reporting violations</h2>
+        <p>To report a violation of this policy, please contact us at communitysaxxys@gmail.com</p>
+        <p>We reserve the right to change this policy at any given time, of which you will be promptly updated. If you want to make sure that you are up to date with the latest changes, we advise you to frequently visit this page.</p>
+      </div>
+    </div>
+  )
+}
