a3968d7f5711db1a0ce03f64f0f9cc8b16cfa40f
Oli Fog <oliver@fogelin.com>
2021-03-15 19:20:12 +0000

    Cookie/privacy policy popup

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar/Navbar'
 import PropTypes from 'prop-types'
-
+import CookieConsent from 'react-cookie-consent'
 import Router from 'next/router'
+import Link from 'next/link'
+
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 NProgress.configure({ showBar: false })
@@ -29,6 +31,26 @@ export default function App ({ Component, pageProps }) {
           quality={100}
         />
       </div>
+      <CookieConsent
+        buttonText="Okay!"
+        cookieName="userAccepted"
+        style={{ background: '#2B373B' }}
+        buttonStyle={{ color: '#120106', fontSize: '14px' }}
+        overlay={true}
+      >
+        This website uses cookies to enhance the user experience!
+        <br />
+        <span className="text-sm space-x-1">
+          <div className="inline-block">
+            By continuing to use our site, you agree to our
+          </div>
+          <Link href="/terms"><a className="text-blue-600 inline-block">terms and conditions</a></Link>
+          <div className="inline-block">
+            /
+          </div>
+          <Link href="/privacypolicy"><a className="text-blue-600 inline-block">privacy policy</a></Link>.
+        </span>
+      </CookieConsent>
       <main>
         <div className="flex flex-col items-center">
           <div className="fixed h-screen w-full max-w-screen-md mx-auto bg-darkred-light z-1"></div>
